89016cd3fea2dad4132d7a3953ed6e2482217408
BuddyBudHead <sen1003f@googlemail.com>
2018-07-04 04:29:33 +1300

    bugfixing in time calculation

--- datecontroller.js
+++ datecontroller.js
@@ -13,21 +13,29 @@ methods.getCurrentTime = function(){
  */
 methods.getCountdownDate = function(){
     var date = new Date();
+    console.log(date.getDay());
+    console.log(date.getHours());
 
     //tuesday - before 7pm
-    if(date.getDay() == 2 && date.getHours >=19){
-        date.setDate(date.getDate() + (2 + (7 - date.getDay()) % 7));
+    if(date.getDay() == 2 && date.getHours() <19){
+        console.log("Dienstag vor 19 Uhr");
+        date.setDate(date.getDate() + (2 + 7 - date.getDay()) % 7);
     }
     //tuesday - after 7pm
-    if(date.getDay() == 2 && date.getHours <19){
-        date.setDate(date.getDate() + 2 + (7 - date.getDay()) % 7);
+    if(date.getDay() == 2 && date.getHours() >=19){
+        console.log("Dienstag nach 19 Uhr");
+        date.setDate(date.getDate() + 2 + 7 - date.getDay()) % 7;
     }
     //every other day
-    else {date.setDate(date.getDate() + (2 + 7 - date.getDay()) % 7);}
+    if(date.getDay() != 2) {
+        console.log("kein Dienstag"); 
+        date.setDate(date.getDate() + (2 + 7 - date.getDay()) % 7);
+    }
     var day = date.getUTCDate();
     var month = date.getUTCMonth()+1;
     var year = date.getUTCFullYear();
     var nexTuesday = month+"/"+day+"/"+year;
+    console.log(nexTuesday);
     //add 19 hours in seconds
     var nexTuesdayTimestamp = new Date(nexTuesday).getTime() + 68400000;
     
