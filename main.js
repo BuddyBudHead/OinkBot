0df24dd50b6a986efcf68a5e820271a3bcc862ef
BuddyBudHead <sen1003f@googlemail.com>
2018-07-08 12:21:19 +0200

    added sound functionality

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
 const filesystem = require('fs');
-const datecontroller = require('./datecontroller.js')
+const datecontroller = require('./datecontroller.js');
+const soundcontroller = require('./soundcontroller.js')
 const config = JSON.parse(filesystem.readFileSync('config.json', 'utf8'));
 
 var DiscordClient = new Discord.Client();
 DiscordClient.login(config.token);
+
+var commands = {
+        'flo'      : 'flo', 
+        'flo-help' : 'flo-help',
+        'oink'     : 'oink'
+    };
 
 DiscordClient.on('ready', function(){
     console.log(DiscordClient.user.username + ": Gestartet.");
@@ -15,13 +22,15 @@ DiscordClient.on('message', function(message){
     // name of the bot
     console.log(DiscordClient.user.username + ": Kein Prefix erkannt.");
 
-    var content = message.content,
-        author  = message.member,
-        channel = message.channel,
-        guild   = message.guild
+    var content      = message.content,
+        author       = message.member,
+        channel      = message.channel,
+        guild        = message.guild,
+        voiceChannel = message.member.voiceChannel
  
-        if(author.id != DiscordClient.user.id && content.startsWith(config.prefix)){
-            console.log("oink");
+        // handle the countdown
+        if(author.id != DiscordClient.user.id && content.startsWith(config.prefix + commands['flo'])){
+
             var currenTime = datecontroller.data.getCurrentTime();
             var countdownDate = datecontroller.data.getCountdownDate();
             var differenceTime = countdownDate - currenTime;
@@ -32,8 +41,29 @@ DiscordClient.on('message', function(message){
 
             generateTimerOutput(channel, differenceTime);
 
-           
+        }
 
+        // handle the oink sound
+        if(author.id != DiscordClient.user.id && content.startsWith(config.prefix + commands['oink'])){
+
+            const permissions = voiceChannel.permissionsFor(message.client.user);
+
+           if (!voiceChannel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+
+           if (!permissions.has('CONNECT')) {
+               return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+           }
+           if (!permissions.has('SPEAK')) {
+               return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+            }
+
+            voiceChannel.join().then(connection =>{
+                const dispatcher = connection.playFile('./floink.mp3');
+  
+                dispatcher.on('end', end => {
+                    voiceChannel.leave();
+                });
+            }).catch(err => console.log(err));            
         }
 });
 
---
+++ soundcontroller.js
@@ -0,0 +1,7 @@
+var methods = {};
+
+methods.oink = function(voiceChannel) {
+console.log(voiceChannel);
+}
+
+exports.data = methods;
