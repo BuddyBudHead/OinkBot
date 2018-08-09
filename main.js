37d902d8e86218086d557e7cba23de5492cd3bbb
BuddyBudHead <sen1003f@googlemail.com>
2018-08-09 16:14:28 +0200

    refactored soundcontroller

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ DiscordClient.on('message', function(message){
         channel      = message.channel,
         guild        = message.guild,
         voiceChannel = author.voiceChannel
-        
+
     console.log(voiceChannel);
 
         // handle the countdown
@@ -49,25 +49,7 @@ DiscordClient.on('message', function(message){
 
         // handle the oink sound
         if(author.id != DiscordClient.user.id && content.startsWith(config.prefix + commands['oink'])){
-
-           if (!voiceChannel) return channel.send('Du musst schon im Call sein, um dir das Oinken zu gönnen.');
-
-           const permissions = voiceChannel.permissionsFor(message.client.user);
-
-           if (!permissions.has('CONNECT')) {
-               return channel.send('Ich darf mich nicht zum Voicechannel verbinden :c');
-           }
-           if (!permissions.has('SPEAK')) {
-               return channel.send('Ich habe nicht die Erlaubis zu sprechen :c');
-            }
-
-            voiceChannel.join().then(connection =>{
-                const dispatcher = connection.playFile('./floink.mp3');
-  
-                dispatcher.on('end', end => {
-                    voiceChannel.leave();
-                });
-            }).catch(err => console.log(err));            
+            soundcontroller.data.oink(voiceChannel, message);            
         }
 
         if(author.id != DiscordClient.user.id && content.startsWith(config.prefix + commands['emoji'])){
--- soundcontroller.js
+++ soundcontroller.js
@@ -1,7 +1,26 @@
 var methods = {};
 
-methods.oink = function(voiceChannel) {
-console.log(voiceChannel);
+methods.oink = function(voiceChannel, message) {
+
+    if (!voiceChannel) return channel.send('Du musst schon im Call sein, um dir das Oinken zu gönnen.');
+
+    const permissions = voiceChannel.permissionsFor(message.client.user);
+
+    if (!permissions.has('CONNECT')) {
+        return channel.send('Ich darf mich nicht zum Voicechannel verbinden :c');
+    }
+    if (!permissions.has('SPEAK')) {
+        return channel.send('Ich habe nicht die Erlaubis zu sprechen :c');
+     }
+
+     voiceChannel.join().then(connection =>{
+         const dispatcher = connection.playFile('./floink.mp3');
+
+         dispatcher.on('end', end => {
+             voiceChannel.leave();
+         });
+     }).catch(err => console.log(err));
+
 }
 
 exports.data = methods;
